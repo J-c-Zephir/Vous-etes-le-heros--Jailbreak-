b51231bcc6e79e6c4e0bfb00a94b66c95346e9b3
J-c-Zephir <jczephir1@gmail.com>
2021-09-29 21:31:46 -0400

    objet

---
+++ script.js
@@ -0,0 +1,11 @@
+let chapitre = {
+    subtitle: "La cellule",
+    text: "Ayant été condamné pour avoir commis un crime violent contre l'humanité, vous vous retrouvez en prison. Sachant que vous être innocent, votre but est de vous enfuir de prison pour prouver votre innoncence. Bonne Chance",
+    img: "prison.jpg",
+  };
+
+let chaptersObj = {
+    slug: "",
+    chapitre: "chaptire",
+
+}
