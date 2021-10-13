6dd77de06b82f0b1ee66baec3579f661e9092067
J-c-Zephir <jczephir1@gmail.com>
2021-10-13 00:34:18 -0400

    3.1 ALMOST
    3.1 est presque prêt sauf le last etape

--- script.js
+++ script.js
@@ -5,49 +5,49 @@
     //CHAPITRE 1
 
 let chaptersObj = {
-    la_prison: chapitre1 = {
+    la_prison: {
         subtitle: "La cellule",
         text: "Ayant été condamné pour avoir commis un crime violent contre l'humanité, vous vous retrouvez en prison. Le cervaux de l'opération est en liberté et tu pourris en prison, tu ne veux pas finir tes jours en prison. Tout d'un coup, tu recois vent d'un plan de s'échapper de prison par les prisonniers. Aujourd'hui est le grand jour est tu prêt? Sachant que vous être innocent, votre but est de vous enfuir de prison pour prouver votre innoncence. Bonne Chance",
-        img: "prison.jpg",
+        img: "/assets/prison.jpg",
         options: [{
             text:"Aller vous coucher ",
             action:'goToChapter(confontation)'
-        }, { //le temps passe
+        }, {
             text:"le temps passe",
-            action:'goToChapter(confontation)'
+            action:"goToChapter(confontation)"
         }]
       },
 
       //CHAPITRE 2
 
-    confontation: chapitre2 = {
+    confontation: {
         subtitle: "La confrontation",
         text: "Un garde apparait et fait les rondes dans les alentours. Vous apercevez des licornes pour une raison curieuse.",
-        img: "confrontation.jpg",
+        img: "/assets/confrontation.jpg",
         options: [{
-            text:"Provoquer garde", //Le garde se fache et vous bat contre vous. vous perdez le combat
+            text:"Provoquer garde", 
             action:'goToChapter(mort)'},
             {
                 text: "Ne rien faire",
-                action: 'goToChapter(choix_difficile)'
+                action:"goToChapter(choix_difficile)"
             }],
     },
-    le_garde: battu = {
+    le_garde: {
         subtitle: "Le Garde te casse la yeuele",
         text: "La souffrance est grande depuis que le garde vous fait des leçon de boxe sans retenir ses coups. Vous etes mort.",
-        img: "boxed_up.jpg",
+        img: "/assets/boxed_up.jpg",
         options: [{
             text:"Rejouer ",
-            action:'goToChapter(la_prison)'
+            action:"goToChapter(la_prison)"
         },]
       },
 
       //CHAPITRE 3
 
-    choix_difficile: chapitre3 = {
+    choix_difficile: {
         subtitle:"choix difficile ",
         text:"En arrivant vous apercevez un biscuit, radioactif vert, une manette qui semble curieuse, et un outil suspicieux qui ressemble drôlement a un fusil.",
-        img:"pourison.jpg ",
+        img:"/assets/pourison.jpg ",
         options: [{
             text:"Manger_Biscuit",
             action:"goToChapter(biscuit)"
@@ -60,19 +60,19 @@ let chaptersObj = {
             action:"goToChapter(laser)"
         }],
     },
-    teleporter: mort_teleporteur = {
+    teleporter: {
         subtitle: "Dans un mur ouch",
         text: "Une fin tragique, vous vous retrouvez dans un mur et incapable d'en sortir. ",
-        img: "pourison.jpg",
+        img: "/assets/teleporter.jpg",
         options: [{
             text:"réesayer",
             action:"goToChapter(la_prison) "
         },]
       },
-    biscuit: manger_biscuit = {
+    biscuit: {
         subtitle: "choix difficile",
         text: "Du temps à passé choisir entre une manette qui semble curieuse, et un outil suspicieux qui ressemble drôlement a un fusil.; ",
-        img: "pourison.jpg",
+        img: "/assets/cookie.jpg",
         options: [{
             text:"Utiliser la machine",
             action:"goToChapter(teleporter) "
@@ -81,10 +81,10 @@ let chaptersObj = {
             action:"goToChapter(un_etre_apparait)"
         }]
       },
-      laser: shooting_range = {
+      laser: {
         subtitle: "Carnage",
         text: "Vous entrez dans une pièce pleine de gardes et vous vous faites remplir de balles. Vous etes mort. ",
-        img: "gun_dead.jpg",
+        img: "/assets/gun_dead.jpg",
         options: [{
             text:"Utiliser la machine",
             action:"goToChapter(mort) "
@@ -96,10 +96,10 @@ let chaptersObj = {
 
       //CHAPITRE 4
 
-    un_etre_apparait: chapitre4 = {
+    un_etre_apparait: {
         subtitle:"Un être Apparaît ",
         text:"Vous appercevez une jeune femme qui à l'air troublé. Vous vous demandez si vous devriez lui parler. ",
-        img:"woman.jpg ",
+        img:"/assets/woman.jpg ",
         options: [{
             text:"Tuer la femme",
             action:"goToChapter(tuer_femme)",
@@ -112,30 +112,30 @@ let chaptersObj = {
         }],
     },
 
-    tuer_femme: shooting_woman = {
+    tuer_femme: {
         subtitle:"Un accident infortuné ",
         text:"Le fusil surchauffe et vous explosez. oups. Vous êtes mort ",
-        img:"grilled_hair.jpg ",
+        img:"/assets/grilled_hair.jpg ",
         options: [{
             text:"Réessayer",
             action:"goToChapter(la_prison)",
         },],
     },
 
-    attendre_femme: waiting_women = {
+    attendre_femme: {
         subtitle:"Oh lala on est pressé la ",
         text:"Une panooplie de gardes viennent vous embusquer et vous mourrez d'humiliation de vous être fait embusquer ",
-        img:"bully.jpg ",
+        img:"/assets/bully.png ",
         options: [{
             text:"reccomencer",
             action:"goToChapter(la_prison)",
         },],
     },
 
-    parler_femme: woman_talking = {
+    parler_femme: {
         subtitle:"Un duo surprenant ",
         text:"Vous lui demander de travailler avec vous et elle accepte! vous êtes maintenant deux pour essayer de s'enfuir. une decision vous attend ",
-        img:"happy_friends.jpg ",
+        img:"/assets/happy_friends.jpg ",
         options: [{
             text:"Se decider",
             action:"goToChapter(la_grande_decision)",
@@ -145,10 +145,10 @@ let chaptersObj = {
 
         //CHAPITRE 5
 
-    la_grande_decision: chapitre5 = {
+    la_grande_decision: {
         subtitle:"La grande decision ",
         text:"Un bouton rouge apparait en face de vous allez vous appuyer dessus oui ou non? ",
-        img:"boutton.jpg ",
+        img:"/assets/boutton.jpg ",
         options: [{
             text:"Appuyer",
             action:"goToChapter(win) "
@@ -158,10 +158,10 @@ let chaptersObj = {
         }],
     },
 
-    la_grande_decision: chapitre5 = {
+    la_grande_decision2: {
         subtitle:"Le bouton n'est pas appuyé ",
         text:"Vous vous faites encercler et capturer par les gardes. GAME OVER ",
-        img:"boutton.jpg ",
+        img:"/assets/boutton.jpg ",
         options: [{
             text:"Réessayer ",
             action:"goToChapter(la_prison) "
@@ -169,10 +169,10 @@ let chaptersObj = {
     },
 
 
-    mort: lose = {
+    mort: {
         subtitle:" Perdant",
         text:"Vous avez perdu",
-        img:"lose.png",
+        img:"/assets/lose.png",
         options: [{
             text:"Réessayer",
             action: 'goToChapter(la_prison)'
@@ -181,10 +181,10 @@ let chaptersObj = {
 
     //WIN SCREEN
 
-    gagner: win = {
+    gagner: {
         subtitle:" Vous avez Gagné !",
         text:"Le système de sécurité s'est désactivé et vous réuissez de vous enfuir et de regagner vos droits. Vous avez gagné bravo ",
-        img:"bravo.jpg",
+        img:"/assets/bravo.jpg",
         options: [{
             text:"Rejouer",
             action:"goToChapter(la_prison)"
@@ -194,7 +194,18 @@ let chaptersObj = {
 
 function goToChapter(chapterName){
     let chapter = chaptersObj[chapterName];
-    console.log(chapter.subtitle)
-    console.log(chapter.text)
-    console.log(chapter.img)
+    console.log(chapter.subtitle);
+    console.log(chapter.text);
+    console.log(chapter.img);
+
+    document.querySelector("#sub_title").innerHTML = chapter.subtitle;
+    document.querySelector("#text_id").innerHTML = chapter.text;
+    document.querySelector("#image").src = chapter.img;
+
+        // for {
+        //     document.querySelector("#action1").onclick= .chapter;
+        //     document.querySelector("#action2").onclick="showMsg();
+        //     document.querySelector("#action3").onclick="showMsg();      
+        // }
+
 }
